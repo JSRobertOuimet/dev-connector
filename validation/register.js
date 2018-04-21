bf938a645a4a0f0da249840f01951c19ebf36040
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-04-20 22:46:49 -0400

    Modified order of registration validation messages.

--- validation/register.js
+++ validation/register.js
@@ -10,21 +10,21 @@ module.exports = function validateRegisterInput(data) {
   data.confirmPassword = !isEmpty(data.confirmPassword) ? data.confirmPassword : "";
 
   // Name
-  if(Validator.isEmpty(data.name)) {
-    errors.name = "Name is required.";
-  }
-
   if(!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters.";
   }
 
-  // Email
-  if(Validator.isEmpty(data.email)) {
-    errors.email = "Email is required.";
+  if(Validator.isEmpty(data.name)) {
+    errors.name = "Name is required.";
   }
 
+  // Email
   if(!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid.";
+  }
+
+  if(Validator.isEmpty(data.email)) {
+    errors.email = "Email is required.";
   }
 
   // Password
