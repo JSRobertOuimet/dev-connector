92796bf77a38e2fff8814c51fba390de1bd24b8a
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-04-21 14:59:35 -0400

    Saved token to local storage.

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
-import { GET_ERRORS } from "./types";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 export const registerUser = (userData, history) => dispatch => {
   axios.post("/api/user/register", userData)
@@ -10,4 +12,31 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+export const loginUser = userData => dispatch => {
+  axios.post("/api/user/login", userData)
+    .then(res => {
+      const { token } = res.data;
+
+      localStorage.setItem("token", token);
+      setAuthToken(token);
+
+      const decoded = jwt_decode(token);
+
+      dispatch(setCurrentUser, decoded);
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
