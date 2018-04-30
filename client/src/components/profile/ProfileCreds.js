d8645b91161c8722edc8379be9c4bf0ed164452b
JSRobertOuimet <jouimet@kivuto.com>
2018-04-30 14:55:35 -0400

    Added profile header

---
+++ client/src/components/profile/ProfileCreds.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProfileCreds extends Component {
+  render() {
+    return (
+      <div>
+        Profile Credentials
+      </div>
+    );
+  }
+};
+
+export default ProfileCreds;
